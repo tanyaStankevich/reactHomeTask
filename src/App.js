90656c8f8214b2b4881cf9d6b1f75345fbc5342c
unknown <tanjastankevich@rambler.ru>
2021-09-26 22:45:05 +0300

    copy repository reactJS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import Message from './Message.js';
 
@@ -7,10 +7,25 @@ import Message from './Message.js';
 const messageText='It is my first react app';
 
 function App (){
+  // const [author, setAuthor] = useState([]);
+  // const [message, setMessage] = useState([]);
+
+  // const updateAuthor = (event) => {
+  //   setAuthor(event.target.value);
+  //   console.log(author);
+  // }
+  // const updateMes = (event) => {
+  //   setMessage(event.target.value);
+  //   console.log(message);
+  // }
+
   return (
       <div>
           <h3 className='app'>Hello!</h3>
-          <Message mes={messageText} />
+      <Message mes={messageText} />
+      {/* <p>Имя отправителя</p><input type='text' author={author} onChangeAuthor={updateAuthor} />
+      <p>Ваше сообщение</p><input type='text' message={message} onChange={updateMes} />
+      <button type='button'>Отправить</button> */}
       </div>
   );
 }
