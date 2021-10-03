6d30153d07734071107468d08d82615629ca771f
unknown <tanjastankevich@rambler.ru>
2021-10-03 18:51:24 +0300

    ls2

--- src/App.js
+++ src/App.js
@@ -2,80 +2,37 @@ import './App.css';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Message from './Message.js';
-
+import MessageChat from './MessageChat';
+import ListChat from './ListChat';
 
 const messageText = 'Have a nice chat';
-const messageList = [];
+
 
 //const messageChat = [];
 
 
-export default class App extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state =
-    {
-      author: '',
-      messageAuthor: ''
-    };
-    this.update = this.update.bind(this.state);
-    this.updatePol = this.updatePol.bind(this.state);
+export default function App() {
   
-  }
-
-  updatePolAuth = (event) => {
-    this.setState({
-            
-      author: event.target.value
-    
-    }
-    );
-  }
-  updatePol = (event) => {
-    this.setState({
-            
-       messageAuthor: event.target.value
-    
-    }
-    );
-  }
-
-  update = (event) => {
-    
-     
-      messageList.push(this.state );
-      console.log(messageList)
-      this.setState({    
-      author: 'Bot',
-      messageAuthor:'I am bot'
-    }
-    );
-    }
+  const author = '';
+  const messageAuthor = '';
   
-    
-    
 
 
   
-  render() {
+  
     
  
   return (
-  
+   <div className='frag'>
+   <ListChat />
       <div>
       <h3 className='app'>Welcome to myMessanger</h3>
       <Message mes={messageText} />
-      <p>Input your name</p>
-      <input type='text' value={this.author} onChange={this.updatePolAuth}></input>
-      <p>Input your message</p>
-      <input type='text' value={this.messageAuthor} onChange={this.updatePol}></input>
-      <button type='button' onClick={this.update}>Send</button><p>чтобы получить ответ бота дважды нажмите на кнопку</p>
-      <ul>Ваш чат {messageList.map((mes) => (<li>Отправитель {mes.author} Сообщение { mes.messageAuthor}</li>))}</ul>
-      </div>  
-    
-    
+      <MessageChat author={author} messageAuthor={messageAuthor} />
+    </div>
+    </div>
   );
-}
+
 }
 
 
