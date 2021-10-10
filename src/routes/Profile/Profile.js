3170c45c22bbcc53552f4c05049eb58569e6be3c
unknown <tanjastankevich@rambler.ru>
2021-10-10 10:31:36 +0300

    some change for lesson 5

--- src/routes/Profile/Profile.js
+++ src/routes/Profile/Profile.js
@@ -4,27 +4,27 @@ import { useCallback} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { store } from "../../store/index";
 import { ProfileReducer, initialState } from "../../store/profile/reducer";
-import { PROFILE_ACTION } from '../../store/profile/actions'
+import { profileAction } from '../../store/profile/actions'
 
 export default function Profile() {
  
   const action = useSelector((state) => {
-    return state.show;
+    return state;
   });
 
   const dispatch = useDispatch();
 
   const setShowName = useCallback(() =>
-    dispatch(PROFILE_ACTION),
+    dispatch(profileAction),
   
     [dispatch])
   
   return (
     <div>
       <h1>Profile</h1>
-      <input type='checkbox' checked={ show} value={show} onChange={setShowName} />
+      <input type='checkbox' checked={action.show} value={action.show} onChange={setShowName} />
 
-      {show && <p>{nameChat}</p>}
+      {action.show && <p>{action.nameChat}</p>}
     </div>
   )
   }
